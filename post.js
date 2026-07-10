08d5643739bc67f2f41210660bdb0707654ac54d
Sruthi A <sruthi8162006@gmail.com>
2026-07-10 22:14:58 +0530

    Models
    The model files define the database structure of the TwinBirds application using Mongoose schemas. They organize user accounts, posts, and comments in MongoDB, ensuring efficient data storage, retrieval, and relationship management between users and their content.
    
    User Model
    
    The User model stores the details of every registered user, including their name, email, encrypted password, bio, profile image, followers, and following lists. It manages user authentication and profile-related information.
    
    Post Model
    
    The Post model stores all posts created by users. It contains the post caption, image, creator information, likes, and timestamps. This model enables users to create, view, and interact with posts.
    
    Comment Model
    
    The Comment model stores comments made on posts. It links each comment to a specific post and user while storing the comment text and creation time. It supports user interaction and discussions on posts.

---
+++ post.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+
+const postSchema = new mongoose.Schema({
+
+    user:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User",
+        required:true
+    },
+
+    caption:{
+        type:String,
+        required:true
+    },
+
+    image:{
+        type:String,
+        default:""
+    },
+
+    likes:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+
+},{
+    timestamps:true
+});
+
+module.exports = mongoose.model("Post",postSchema);
---
+++ user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+{
+    name:{
+        type:String,
+        required:true
+    },
+
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+
+    password:{
+        type:String,
+        required:true
+    },
+
+   profileImage: {
+    type: String,
+    default: ""
+},
+    bio:{
+        type:String,
+        default:"Hello! I am using CodeAlpha Social."
+    },
+
+    followers:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ],
+
+    following:[
+        {
+            type:mongoose.Schema.Types.ObjectId,
+            ref:"User"
+        }
+    ]
+},
+{
+    timestamps:true
+});
+
+module.exports = mongoose.model("User",userSchema);
